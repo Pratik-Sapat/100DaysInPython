78e673031488185a3bf6d4dd560edbc680159f49
Pratik Sapat <87898402+Pratik-Sapat@users.noreply.github.com>
2023-03-03 14:22:38 +0530

    Day10_Programs

---
+++ Day10/1_every_first_capital.py
@@ -0,0 +1,21 @@
+def format_name(f_name, l_name):
+    """Takes first and last name and format it in string
+     - Doc string example."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    #end of function
+    return f"{formatted_f_name} {formatted_l_name}" 
+   
+
+
+f_string = format_name("PraTik", "SaPAT") 
+print(f_string)
+
+#you can ask for input also.
+f_string = format_name(input("First name : "), input("Last name : "))
+print(f_string)
+
+#title function can make every first word capital in  sentence;
