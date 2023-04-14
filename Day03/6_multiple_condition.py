9dffd289979d46c205fd6a4b90571c01f828180d
Pratik Sapat <87898402+Pratik-Sapat@users.noreply.github.com>
2023-04-14 13:48:32 +0530

    Project : love_calculator & Tresure_island & Pizza order

---
+++ Day03/6_multiple_condition.py
@@ -0,0 +1,28 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("Height in CM? "))
+bill = 0
+if height > 120:
+    print("YOu can ride rollercoaster!")
+    age = int(input("What is your age? "))
+
+    if  age<12:
+        bill = 5
+        print("Please pay $5.")
+    elif age <= 18:
+        bill = 7
+        print("please pay $7.")
+    else:
+        bill = 12
+        print("please pay $12.")
+    wants_phot = input("Do you want a photo taken ? y or n")
+    if wants_phot == "y":
+        bill +=  3
+
+    print(f"your final bill is ${bill}")
+
+
+else:
+    print("Sorry ,you have to grow taller before!")
+
+    
