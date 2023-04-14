f4918b9e213c1a246677a68efcc38d27b74d89f2
Pratik Sapat <87898402+Pratik-Sapat@users.noreply.github.com>
2023-04-14 13:55:21 +0530

    project : Ceasar_cipher & Prime_Checker

---
+++ Day08/2_multiple_input.py
@@ -0,0 +1,8 @@
+def greet_with(name, location): #parameters
+    print(f"Hello {name}")
+    print(f"how is it like in {location}")
+
+greet_with("pratik", "Nagpur") #positional arguments
+
+
+# greet_with(location = "Nagpur", name = "pratik")
