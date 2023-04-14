f1db1912374a3be5d15e3ffca6ab3eff0adb3c14
Pratik Sapat <87898402+Pratik-Sapat@users.noreply.github.com>
2023-04-14 13:44:34 +0530

    Day2_Programs
    project : - tip-calculator and how many days left.

---
+++ Day02/2_1.py
@@ -0,0 +1,7 @@
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+
+
+print(str(70)+str(100))
---
+++ Day02/7.py
@@ -0,0 +1,9 @@
+# f-string
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score} with height {height} your winning is {isWinning}")
+
+
