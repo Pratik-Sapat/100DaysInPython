056070f17c36e358b9f41553339d57593c62c919
Pratik Sapat <87898402+Pratik-Sapat@users.noreply.github.com>
2023-04-14 13:46:19 +0530

    Day1_Programs
    basics

---
+++ Day01/eight.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator.")
+
+city=input("which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("your band name could be "+ city +" "+ pet)
