f12a30adbe49b742700e3046dfaa3ca56ec62ba9
Pratik Sapat <87898402+Pratik-Sapat@users.noreply.github.com>
2023-04-14 13:45:18 +0530

    Delete Day1 directory

--- Day1/fifth.py
+++
@@ -1,3 +0,0 @@
-# input("What is your name? ")
- 
-print(len(input("What is your name? ")))
--- Day1/fourth.py
+++
@@ -1,5 +0,0 @@
-print("Hello " + input("What is you name? : ") + "!")
-
-#comments
-# input will get user input in console
-#then print will print the word "hello and the user input."
